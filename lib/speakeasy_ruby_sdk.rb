bae4fbdefe7e6cad21189e43a0bcafb10b8a5acd
Ian Bentley <ian@idbentley.com>
2023-01-27 12:49:13 -0500

    add configurability

--- lib/speakeasy_ruby_sdk.rb
+++ lib/speakeasy_ruby_sdk.rb
@@ -1,13 +1,18 @@
 require 'speakeasy_ruby_sdk/version'
+require 'speakeasy_ruby_sdk/config'
 
 module SpeakeasyRubySdk
   class Middleware
-    def initialize(app)
+    attr_reader :config
+
+    def initialize(app, config=nil)
+      @config = Middleware::Config.default.merge config
       @app = app
     end
    
     def call(env)
       puts 'Middleware reporting in!'
+      puts "Server url #{@config.ingestion_server_url} #{@config.speakeasy_version}"
 
       status, headers, response_body = @app.call(env)
 
---
+++ lib/speakeasy_ruby_sdk/config.rb
@@ -0,0 +1,28 @@
+module SpeakeasyRubySdk
+  class Middleware
+    class Config
+      attr_accessor :ingestion_server_url, :speakeasy_version, :api_key, :api_id, :version_id
+
+      def self.attributes
+        @attributes
+      end
+
+      def self.default
+        c = Config.new
+        c.ingestion_server_url = "grpc.prod.speakeasyapi.dev:443"
+        c.speakeasy_version = "1.5.0"
+        c
+      end
+
+      def merge(config)
+        if config
+          if config.class == Hash
+            config.each { |k, v| instance_variable_set "@#{k}", v }
+          end
+        end
+        self
+      end
+
+    end
+  end
+end
