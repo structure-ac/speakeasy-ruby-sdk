ca3e8cc667c9953878cd286412fea1cbb9b3b259
Ian Bentley <ian@idbentley.com>
2023-02-13 10:45:54 -0700

    handle port correctly and guard empty query params

--- lib/speakeasy_ruby_sdk/url_utils.rb
+++ lib/speakeasy_ruby_sdk/url_utils.rb
@@ -2,13 +2,18 @@ require 'uri'
 module SpeakeasyRubySdk
   module UrlUtils
     def self.resolve_url env, query_params=nil
-      ## TODO: handle Forwarded headers
-      
-      request_uri = env['REQUEST_URI']
-      request_uri = request_uri.split('?')[0]
+      if env.include?('SERVER_PORT') && env['SERVER_PORT'] != "80"
+        request_uri = "#{env['SERVER_NAME']}:#{env['SERVER_PORT']}#{env['PATH_INFO']}"
+      else
+        request_uri = "#{env['SERVER_NAME']}#{env['PATH_INFO']}"
+      end
+
+      if request_uri.include? '?'
+        request_uri = request_uri.split('?')[0]
+      end
       scheme = env['rack.url_scheme']
       host = env['HTTP_HOST']
-      if query_params
+      if !query_params.nil? && !query_params.empty?
         updated_query_string = URI.encode_www_form query_params
         URI("#{scheme}://#{host}#{request_uri}?#{updated_query_string}")
       else 
